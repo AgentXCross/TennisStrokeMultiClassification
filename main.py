b1406cae6182b66d662045571ca2cad83878fce8
Michael Liu <liumichaeler@gmail.com>
2025-09-16 23:02:16 -0400

    training file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import StepLR
-from model import TennisStrokeClassifier
+from new_model import return_model
 from dataset import get_dataloaders
 from train_test_loop import train_test_loop
 from extra_functions import accuracy_fn
@@ -10,9 +10,9 @@ from extra_functions import accuracy_fn
 device = 'mps' if torch.backends.mps.is_available() else 'cpu'
 
 #Initialize Model, Optimizer, Scheduler, Loss Function
-model = TennisStrokeClassifier().to(device)
+model = return_model().to(device)
 
-sgd_optimizer = torch.optim.SGD(params = model.parameters(), lr = 0.01, momentum = 0.9, weight_decay = 1e-4)
+sgd_optimizer = torch.optim.SGD(params = model.parameters(), lr = 0.01, momentum = 0.9, weight_decay = 5e-4)
 
 sgd_scheduler = StepLR(
     sgd_optimizer,  
@@ -28,7 +28,7 @@ train_loader, test_loader = get_dataloaders(batch_size = 32)
 #Train and Testing
 train_test_loop(
     model = model,
-    epochs = 51,
+    epochs = 5,
     device = device,
     optimizer = sgd_optimizer,
     scheduling_function = sgd_scheduler,
@@ -36,7 +36,7 @@ train_test_loop(
     train_dataloader = train_loader,
     test_dataloader = test_loader,
     accuracy_fn = accuracy_fn,
-    seed = 11
+    seed = 77
 )
 
 # Save the trained model
